e94757c8bb59be95137068ab4d07b6d6c2a598b8
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-18 15:58:59 +0200

    Added annotations: @GetMapping and @PutMapping (@RequestMapping)

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -12,12 +11,32 @@ public class HelloController {
     -> It also carries information about the URL where the API will be accessible.
      */
 
-    @GetMapping("/sayHello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello from the controller";
     }
 
-    // -> http://localhost:8080/sayHello
+    /* http://localhost:8080/sayHello
+    #IMPORTANT -> Always re-run after a change
+    */
+
+
+    @PostMapping("/hello")
+    public String createNewHello() {
+        return "Hello was created";
+    }
+
+    //@PutMapping(value = "hello") -> or I can use the annotation below instead
+    @RequestMapping(method = RequestMethod.GET, value = "hello")
+    public String updateHello() {
+        return "Hello is updated";
+    }
+
+    /* Verify if working via cmd:
+       -> curl localhost:8080/hello (result: Hello from the controller - above)
+       -> curl -X POST localhost:8080/hello (result: Hello was created - below)
+       OR - better use POSTMAN to check API
+     */
 
 
 
