f931fede04d43a8e6d41010fc8cb933bf0f2fac4
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-08-08 14:44:22 +0200

    Controller ->
    @GetMapping - return the methods or data
    
    @PostMapping - Creates a new resource or performs an action that changes the state. (Creating a new method or record)
    
    @PutMapping  - Updates or replaces an existing resource. (Changing an existing method or record)

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,26 +11,10 @@ public class HelloController {
     -> It also carries information about the URL where the API will be accessible.
      */
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from the controller";
-    }
-
     /* http://localhost:8080/sayHello
     #IMPORTANT -> Always re-run after a change
+    GET is used to retrieve data, POST to create new resources and PUT to update existing resources.
     */
-
-
-    @PostMapping("/hello")
-    public String createNewHello() {
-        return "Hello was created";
-    }
-
-    //@PutMapping(value = "hello") -> or I can use the annotation below instead
-    @RequestMapping(method = RequestMethod.GET, value = "hello")
-    public String updateHello() {
-        return "Hello is updated";
-    }
 
     /* Verify if working via cmd:
        -> curl localhost:8080/hello (result: Hello from the controller - above)
@@ -38,6 +22,19 @@ public class HelloController {
        OR - better use POSTMAN to check API
      */
 
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from the controller";
+    }
 
+    @PostMapping("/hello")
+    public String createNewHello() {
+        return "Hello was created";
+    }
+
+    @PutMapping(value = "/hello/update")
+    public String updateHello() {
+        return "Hello is updated";
+    }
 
 }
