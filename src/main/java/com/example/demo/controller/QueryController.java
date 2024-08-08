fed95b1e11a5990bfbe64687a3fb5d0bac2cb3a8
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-08-08 16:21:19 +0200

    the second example of the method @GetMapping -> ageClient added

--- src/main/java/com/example/demo/controller/QueryController.java
+++ src/main/java/com/example/demo/controller/QueryController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QueryController {
 
+    //1. example @GetMapping with one parameter. Can be also -> , default value = " -,,- "
     @GetMapping("/v1/name")
     public String sayWelcomeClient(@RequestParam(value = "FullName", required = false) String NameOfClient) {
       //return "Hello new Client, " + NameOfClient;
@@ -14,6 +15,15 @@ public class QueryController {
       } else {
           return "Welcome new client, " + NameOfClient;
       }
+    }
+
+    //2. example @GetMapping with two parameters
+    @GetMapping("/v1/client/age")
+    public String ageClient(
+            @RequestParam(name = "surname") String nameOfClient,
+            @RequestParam(name = "age") int ageOfClient)
+    {
+        return "The Client " + nameOfClient + " is " + ageOfClient + " years old.";
     }
 
 
